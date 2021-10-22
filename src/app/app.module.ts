8b83cd7328cd82cb81e746f5f124246bd34feb5d
Franweb79 <franprietogutweb@gmail.com>
2021-10-23 00:15:37 +0200

    divs showing results adjusted with z index to prepare the flip background effect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './componentes/home-component/componente-home.component';
 import { HeaderComponent } from './componentes/header-menu-component/header-menu-component.component';
 import { ErrorComponent } from './componentes/error-component/error-component.component';
+import { ShowDaysComponent } from './componentes/show-days-component/show-days/show-days.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    ErrorComponent
+    ErrorComponent,
+    ShowDaysComponent
   ],
   imports: [
     BrowserModule,
