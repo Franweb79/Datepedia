330f32d253b2dd47698de7915f899e218a2d9410
Franweb79 <franprietogutweb@gmail.com>
2021-11-26 18:41:49 +0100

    changed favicon and title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Days between dates calculator';
+  title = 'Datepedia';
 }
